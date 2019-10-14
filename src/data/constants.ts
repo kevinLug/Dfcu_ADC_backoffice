b56e8a52c7123ad3d1e6496b7188546584c596e3
Timothy Emmanuel Kasasa <timothyk@laboremus.no>
2019-10-14 12:34:46 +0300

    Added  environment files

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -34,15 +34,62 @@ export const url = debug ? 'http://localhost:6001' :
     'https://emata-poc-workeng-test.laboremus.no'
 
 
-export const remoteRoutes = {
-    authServer: 'https://authservice-test.laboremus.no',
-    login: url + '/api/test/login',
-    profile: url + '/api/test/profile',
-    register: url + '/api/auth/register',
-    resetPass: url + '/reset',
+const servers: any = {
+    dev: {
+        Auth: 'https://authservice-test.laboremus.no',
+        Crm: 'https://crmservice-test.laboremus.no',
+        Case: 'http://localhost:6001',
+        Kyc: "https://kyc-connector-staging.onboarding.dfcugroup.com",
+        Notification: "https://emata-poc-smsservice-test.laboremus.no",
+        Payments: "https://emata-poc-payments-test.laboremus.no",
+    },
+    test: {
+        Auth: 'https://authservice-test.laboremus.no',
+        Crm: 'https://emata-poc-crmservice-test.laboremus.no',
+        Case: 'https://emata-poc-workeng-test.laboremus.no',
+        Kyc: "https://kyc-connector-staging.onboarding.dfcugroup.com",
+        Notification: "https://emata-poc-smsservice-test.laboremus.no",
+        Payments: "https://emata-poc-payments-test.laboremus.no",
+    },
+    staging: {
+        Auth: "",
+        Crm: "",
+        Case: "",
+        Kyc: "",
+        Notification: "",
+        Payments: ""
+    },
+    production: {
+        Auth: "",
+        Crm: "",
+        Case: "",
+        Kyc: "",
+        Notification: "",
+        Payments: ""
+    }
+}
 
-    contacts: url + '/api/crm/contact',
-    workflows: url + '/api/workflows',
+const evVar = process.env.REACT_APP_ENV || 'dev'
+const environment = evVar.trim()
+console.log(`############# Env : ${environment} ###############`)
+const env = servers[environment]
+const authURL = env.Auth
+const crmURL = env.Crm
+const caseHandlingURL = env.Case
+const gatewayURL = env.Gateway
+const kycURL = env.Kyc
+const notificationURL = env.Notification
+
+
+export const remoteRoutes = {
+    authServer: authURL,
+    login: authURL + '/api/test/login',
+    profile: authURL + '/api/test/profile',
+    register: authURL + '/api/auth/register',
+    resetPass: authURL + '/reset',
+
+    contacts: crmURL + '/api/crm/contact',
+    workflows: caseHandlingURL + '/api/workflows',
 }
 
 
