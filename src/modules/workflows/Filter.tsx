50ec4f89912bcb55b94590446eca0e4bb70de882
Daniel Comboni <daniel@laboremus.no>
2021-05-03 13:17:22 +0300

    added more ellipsis for filter fields

--- src/modules/workflows/Filter.tsx
+++ src/modules/workflows/Filter.tsx
@@ -1,34 +1,117 @@
 import * as React from "react";
-import { useState } from "react";
-import { flatMap, uniqBy } from "lodash";
+import {useEffect, useState} from "react";
+import {flatMap, uniqBy} from "lodash";
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import { IOption, toOptions } from "../../components/inputs/inputHelpers";
-import { Box } from "@material-ui/core";
+import {IOption, toOptions} from "../../components/inputs/inputHelpers";
+import {Box, Menu, MenuProps} from "@material-ui/core";
 import TextField from '@material-ui/core/TextField';
 import PSelectInput from "../../components/plain-inputs/PSelectInput";
 import PDateInput from "../../components/plain-inputs/PDateInput";
-import { enumToArray, getRandomStr } from "../../utils/stringHelpers";
-import { IWorkflowFilter, WorkflowStatus, WorkflowSubStatus } from "./types";
-import { workflowTypes } from "./config";
-import { PRemoteSelect } from "../../components/inputs/XRemoteSelect";
-import { remoteRoutes } from "../../data/constants";
-import { useSelector } from "react-redux";
-import { IState } from "../../data/types";
-import { downLoad, triggerDownLoad } from "../../utils/ajax";
+import {enumToArray, getRandomStr} from "../../utils/stringHelpers";
+import {IWorkflowFilter, WorkflowStatus, WorkflowSubStatus} from "./types";
+import {workflowTypes} from "./config";
+import {PRemoteSelect} from "../../components/inputs/XRemoteSelect";
+import {remoteRoutes} from "../../data/constants";
+import {useSelector} from "react-redux";
+import {IState} from "../../data/types";
+import {downLoad, triggerDownLoad} from "../../utils/ajax";
+import {IList, List} from "../../utils/collections/list";
+import CheckBoxTemplate, {IPropsChecks} from "../scan/validation/Check";
+
+import MenuItem from "@material-ui/core/MenuItem";
+
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import {Theme, withStyles} from "@material-ui/core/styles";
+import createStyles from "@material-ui/core/styles/createStyles";
+
+import IconButton from "@material-ui/core/IconButton";
+
+import {ICheckKeyValueState} from "../../data/redux/checks/reducer";
+import {MoreHoriz} from "@material-ui/icons";
+
+
+const StyledMenu = withStyles({
+    paper: {
+        border: '1px solid #d3d4d5',
+    },
+})((props: MenuProps) => (
+    <Menu
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+        }}
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+        }}
+        {...props}
+    />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+    root: {
+        '&:focus': {
+            backgroundColor: theme.palette.primary.main,
+            '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+                color: theme.palette.common.white,
+            },
+        },
+    },
+}))(MenuItem);
 
 interface IProps {
     onFilter: (data: any) => any
     loading: boolean
 }
 
-const Filter = ({ onFilter, loading }: IProps) => {
+// const useStyles = makeStyles((theme: Theme) =>
+//     createStyles({
+//         button: {
+//             display: 'block',
+//             marginTop: theme.spacing(2),
+//         },
+//         formControl: {
+//             // margin: theme.spacing(1),
+//             // minWidth: 120,
+//             width: '100%'
+//         },
+//     }),
+// );
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        formControl: {
+            width: '100%'
+            // margin: theme.spacing(1),
+            // minWidth: 120,
+        },
+        selectEmpty: {
+            marginTop: theme.spacing(2),
+        },
+        filterGrids: {},
+        exportBtn: {
+            marginLeft: 'auto'
+        }
+    }),
+);
+
+
+const Filter = ({onFilter, loading}: IProps) => {
+
+    const classes = useStyles();
 
     const metaData = useSelector((state: IState) => state.core.metadata)
-    const accountCategories: IOption[] = uniqBy(flatMap(metaData.transferCategories, it => it.accounts.map(({ code, name }) => ({
+    const accountCategories: IOption[] = uniqBy(flatMap(metaData.transferCategories, it => it.accounts.map(({
+                                                                                                                code,
+                                                                                                                name
+                                                                                                            }) => ({
         label: name,
         value: code
     }))), "value")
+
     const [data, setData] = useState<IWorkflowFilter>({
         from: null,
         to: null,
@@ -42,6 +125,49 @@ const Filter = ({ onFilter, loading }: IProps) => {
         idNumber: '',
         userId: ''
     })
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const gridSize: any = {
+        xs: 6,
+        sm: 3,
+        md: 2,
+        lg: 2,
+        xl: 1
+    }
+
+    const {check}: ICheckKeyValueState = useSelector((state: any) => state.checks)
+
+    useEffect(() => {
+
+        console.log(check.checks)
+    }, [check])
+
+    const ids = ['status-grid', 'subStatus-grid', 'refNumber-grid', 'from-grid', 'to-grid']
+    const labels = ['Status', 'Sub Status', 'Ref. Number', 'From Date', 'To Date']
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const checkListShowMoreOrLess = (): IList<IPropsChecks> => {
+
+        const theCheckList = new List<IPropsChecks>();
+
+        labels.forEach((aLabel, index) => {
+            const aCheck: IPropsChecks = {
+                label: aLabel,
+                value: true,
+                name: ids[index]
+            }
+            theCheckList.add(aCheck);
+        })
+
+        return theCheckList;
+    }
 
     function submitForm(values: any) {
         onFilter(values)
@@ -50,7 +176,7 @@ const Filter = ({ onFilter, loading }: IProps) => {
     function handleChange(event: React.ChangeEvent<any>) {
         const name = event.target.name
         const value = event.target.value
-        const newData = { ...data, [name]: value }
+        const newData = {...data, [name]: value}
         setData(newData)
         submitForm(newData)
     }
@@ -59,14 +185,14 @@ const Filter = ({ onFilter, loading }: IProps) => {
         if (name === 'from' || name === 'to') {
             value = value ? value.toISOString() : value
         }
-        const newData = { ...data, [name]: value }
+        const newData = {...data, [name]: value}
         setData(newData)
         submitForm(newData)
     }
 
     const handleComboValueChange = (name: string) => (value: any) => {
-        const newData = { ...data, [name]: value }
-        const newFilterData = { ...data, [name]: value ? value.id : null }
+        const newData = {...data, [name]: value}
+        const newFilterData = {...data, [name]: value ? value.id : null}
         setData(newData)
         submitForm(newFilterData)
     }
@@ -80,16 +206,16 @@ const Filter = ({ onFilter, loading }: IProps) => {
     return <form>
         <Grid spacing={2} container>
 
-            <Grid item xs={1}>
+            <Grid item xs={gridSize.xs} sm={gridSize.sm} md={gridSize.md} lg={gridSize.lg} xl={gridSize.xl}>
                 <PRemoteSelect
                     name="applicant"
                     value={data['applicant']}
                     onChange={handleComboValueChange('applicant')}
                     label="Applicant"
                     remote={remoteRoutes.workflowsCombo}
-                    parser={({ id, name }: any) => ({ id, label: name })}
+                    parser={({id, name}: any) => ({id, label: name})}
                     textFieldProps={
-                        { variant: "outlined", size: "small" }
+                        {variant: "outlined", size: "small"}
                     }
                     filter={{
                         'IdField': 'MetaData.ApplicantName',
@@ -98,16 +224,16 @@ const Filter = ({ onFilter, loading }: IProps) => {
                 />
             </Grid>
 
-            <Grid item xs={1}>
+            <Grid item xs={gridSize.xs} sm={gridSize.sm} md={gridSize.md} lg={gridSize.lg} xl={gridSize.xl}>
                 <PRemoteSelect
                     name="userId"
                     value={data['userId']}
                     onChange={handleComboValueChange('userId')}
                     label="Beneficiary"
                     remote={remoteRoutes.workflowsCombo}
-                    parser={({ id, name }: any) => ({ id, label: name })}
+                    parser={({id, name}: any) => ({id, label: name})}
                     textFieldProps={
-                        { variant: "outlined", size: "small" }
+                        {variant: "outlined", size: "small"}
                     }
                     filter={{
                         'IdField': 'UserId',
@@ -116,7 +242,7 @@ const Filter = ({ onFilter, loading }: IProps) => {
                 />
             </Grid>
 
-            <Grid item xs={1}>
+            <Grid item xs={gridSize.xs} sm={gridSize.sm} md={gridSize.md} lg={gridSize.lg} xl={gridSize.xl}>
                 <PSelectInput
                     name="workflowTypes"
                     value={data['workflowTypes']}
@@ -129,7 +255,8 @@ const Filter = ({ onFilter, loading }: IProps) => {
                 />
             </Grid>
 
-            <Grid item xs={1}>
+            <Grid item xs={gridSize.xs} sm={gridSize.sm} md={gridSize.md} lg={gridSize.lg} xl={gridSize.xl}
+                  id="status-grid" hidden={check.checks.get("status-grid")}>
                 <PSelectInput
                     name="statuses"
                     value={data['statuses']}
@@ -142,7 +269,8 @@ const Filter = ({ onFilter, loading }: IProps) => {
                 />
             </Grid>
 
-            <Grid item xs={1}>
+            <Grid item xs={gridSize.xs} sm={gridSize.sm} md={gridSize.md} lg={gridSize.lg} xl={gridSize.xl}
+                  id="subStatus-grid" hidden={check.checks.get("subStatus-grid")}>
                 <PSelectInput
                     name="subStatuses"
                     value={data['subStatuses']}
@@ -155,7 +283,8 @@ const Filter = ({ onFilter, loading }: IProps) => {
                 />
             </Grid>
 
-            <Grid item xs={1}>
+            <Grid item xs={gridSize.xs} sm={gridSize.sm} md={gridSize.md} lg={gridSize.lg} xl={gridSize.xl}
+                  id="refNumber-grid" hidden={check.checks.get("refNumber-grid")}>
                 <TextField
                     name="referenceNumber"
                     value={data['referenceNumber']}
@@ -168,7 +297,8 @@ const Filter = ({ onFilter, loading }: IProps) => {
                 />
             </Grid>
 
-            <Grid item xs={1}>
+            <Grid item xs={gridSize.xs} sm={gridSize.sm} md={gridSize.md} lg={gridSize.lg} xl={gridSize.xl}
+                  id="from-grid" hidden={check.checks.get("from-grid")}>
                 <PDateInput
                     name="from"
                     value={data['from'] || null}
@@ -179,7 +309,8 @@ const Filter = ({ onFilter, loading }: IProps) => {
                 />
             </Grid>
 
-            <Grid item xs={1}>
+            <Grid item xs={gridSize.xs} sm={gridSize.sm} md={gridSize.md} lg={gridSize.lg} xl={gridSize.xl} id="to-grid"
+                  hidden={check.checks.get("to-grid")}>
                 <PDateInput
                     name="to"
                     value={data['to'] || null}
@@ -190,7 +321,43 @@ const Filter = ({ onFilter, loading }: IProps) => {
                 />
             </Grid>
 
-            <Grid item xs={4}>
+            <Grid item xs={gridSize.xs} sm={gridSize.sm} md={gridSize.md} lg={gridSize.lg} xl={gridSize.xl}>
+
+                <Box>
+                    <IconButton
+                        aria-label="more"
+                        aria-controls="long-menu"
+                        aria-haspopup="true"
+                        onClick={handleClick}
+                        size='small'
+                    >
+                        <MoreHoriz/>
+                    </IconButton>
+                    <StyledMenu
+                        id="customized-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+
+                        {
+                            checkListShowMoreOrLess().toArray().map((more, index) => {
+                                return <StyledMenuItem key={ids[index]}>
+                                    <CheckBoxTemplate value={more.value} label={more.label}
+                                                      name={more.name}/>
+                                </StyledMenuItem>
+                            })
+                        }
+
+                    </StyledMenu>
+
+                </Box>
+
+            </Grid>
+
+            <Grid item xs={gridSize.xs} sm={gridSize.sm} md={gridSize.md} lg={gridSize.lg} xl={gridSize.xl}
+                  className={classes.exportBtn}>
                 <Box display="flex" flexDirection="row-reverse">
                     <Button
                         disabled={loading}
