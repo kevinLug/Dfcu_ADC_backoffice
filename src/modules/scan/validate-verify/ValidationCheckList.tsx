251c5874f30a5d1bc2fc1294cb43c20d090301aa
Daniel Comboni <daniel@laboremus.no>
2021-05-10 07:27:47 +0300

    Cleaned up ValidationCheckList

--- src/modules/scan/validate-verify/ValidationCheckList.tsx
+++ src/modules/scan/validate-verify/ValidationCheckList.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from "react";
 import Grid from "@material-ui/core/Grid";
-import {IPropsChecks} from "./Check";
+import {addCheck, IPropsChecks} from "./Check";
 import CheckBoxTemplate from "./Check";
 import {IList, List} from "../../../utils/collections/list";
 import Box from "@material-ui/core/Box";
@@ -17,9 +17,8 @@ import {remoteRoutes} from "../../../data/constants";
 
 import {post} from "../../../utils/ajax";
 import {getChecksToPopulate} from "../populateLabelAndValue";
-import {fetchWorkflowAsync, startWorkflowFetch} from "../../../data/redux/workflows/reducer";
+import {IWorkflowState} from "../../../data/redux/workflows/reducer";
 import {Dispatch} from "redux";
-import {startNewTransferRequest} from "../../../data/redux/coreActions";
 
 const useStyles = makeStyles(() =>
     createStyles({
@@ -38,25 +37,51 @@ const useStyles = makeStyles(() =>
     })
 );
 
-const ValidationCheckList = () => {
+interface IProps {
+    theCheckList: IList<IPropsChecks>;
+    caseId?: any
+}
+
+export const checkListCSO = (): IList<IPropsChecks> => {
+
+    const theCheckList = new List<IPropsChecks>();
+    theCheckList.add(addCheck("Transfer request is signed as per account mandate", "isTransferSignedAsPerAccountMandate"))
+    theCheckList.add(addCheck("Transfer requires forex", "transferRequiresForex"))
+    theCheckList.add(addCheck("Sender's account number is correct", "isSenderAccountNumberCorrect"))
+    theCheckList.add(addCheck("Sender has sufficient funds", "senderHasSufficientFunds"))
+    theCheckList.add(addCheck("Recipient's bank details are complete", "isRecipientBankDetailsComplete"))
+    theCheckList.add(addCheck("Recipient's physical address is complete (TTs)", "isRecipientPhysicalAddressComplete"))
+
+    return theCheckList;
+}
+
+const ValidationCheckList = ({theCheckList}:IProps) => {
 
     const classes = useStyles()
     const {workflowResponseMessage}: IWorkflowResponseMessageState = useSelector((state: any) => state.workflowResponse)
-    // const [data] = useState()
+
     const {check}: ICheckKeyValueState = useSelector((state: any) => state.checks)
+    const {workflow}: IWorkflowState = useSelector((state: any) => state.workflows)
 
     const dispatch: Dispatch<any> = useDispatch();
 
     useEffect(() => {
         // console.log(check.checks)
-    }, [check])
+    }, [dispatch,check,workflow])
 
     const handleCSOApproval = async () => {
 
         let data = getChecksToPopulate(check.checks);
+        let caseId: string
+        if (!workflowResponseMessage.caseId || workflowResponseMessage.caseId.includes("0000-0000") ){
+            // @ts-ignore
+            caseId = workflow.id
+        }else {
+            caseId = workflowResponseMessage.caseId
+        }
 
         const manualCSOApproval: IManualDecision = {
-            caseId: workflowResponseMessage.caseId,
+            caseId: caseId,
             taskName: "cso-approval", // todo ...consider making these constants
             actionName: "cso-transfer-details-approval",
             resumeCase: true,
@@ -69,41 +94,17 @@ const ValidationCheckList = () => {
                 console.log(resp) // todo ... consider providing a message for but success and failure
             }, undefined,
             () => {
-                window.location.reload()
+                window.location.href = window.location.origin
             }
         )
-    }
-
-    const addCheck = (label: string, name: string, value: boolean = false) => {
-        const theCheckObject: IPropsChecks = {
-            label: label,
-            name: name,
-            value: value
-        }
-        return theCheckObject;
-    }
-
-    const checkList = (): IList<IPropsChecks> => {
-
-        const theCheckList = new List<IPropsChecks>();
-        theCheckList.add(addCheck("Transfer request is signed as per account mandate", "isTransferSignedAsPerAccountMandate"))
-        theCheckList.add(addCheck("Transfer requires forex", "transferRequiresForex"))
-        theCheckList.add(addCheck("Sender's account number is correct", "isSenderAccountNumberCorrect"))
-        theCheckList.add(addCheck("Sender has sufficient funds", "senderHasSufficientFunds"))
-        theCheckList.add(addCheck("Recipient's bank details are complete", "isRecipientBankDetailsComplete"))
-        theCheckList.add(addCheck("Recipient's physical address is complete (TTs)", "isRecipientPhysicalAddressComplete"))
-
-        // console.log(theCheckList)
-
-        return theCheckList;
     }
 
     return (
 
         <Grid container>
             {
-                checkList().toArray().map((aCheck, index) => {
-                    console.log(aCheck.value)
+                theCheckList.toArray().map((aCheck, index) => {
+
                     return <Grid key={index} item sm={12}>
                         <CheckBoxTemplate value={aCheck.value} label={aCheck.label} name={aCheck.name}/>
                     </Grid>
