a5acd1f20387f8758514668139f9a6b9d8b39621
Daniel Comboni <daniel@laboremus.no>
2021-07-09 09:58:17 +0300

    CMO remarks added

--- src/modules/scan/validate-verify/rejection-remarks-values.ts
+++ src/modules/scan/validate-verify/rejection-remarks-values.ts
@@ -71,15 +71,15 @@ export const BMORejectionRemarks = () => {
 
 export const CMORejectionRemarks = () => {
     const remarks = new List<string>();
-    remarks.add("CSO missed something...");
-    remarks.add("BMO missed something...");
-    // remarks.add("Instruction is not signed as per mandate");
-    // remarks.add("Sender's account number is invalid");
-    // remarks.add("Sender has insufficient funds");
-    // remarks.add("The recipient's details are incomplete");
-    // remarks.add("Recipient's physical address is missing");
-    // remarks.add("Forex details are incorrect");
-    // remarks.add("Callbacks where not done"); // todo...who is supposed to do the callbacks...guess it's CSO
+    // remarks.add("CSO missed something...");
+    // remarks.add("BMO missed something...");
+    remarks.add("Instruction is not signed as per mandate");
+    remarks.add("Sender's account number is invalid");
+    remarks.add("Sender has insufficient funds");
+    remarks.add("The recipient's details are incomplete");
+    remarks.add("Recipient's physical address is missing");
+    remarks.add("Forex details are incorrect");
+    remarks.add("Callbacks where not done"); // todo...who is supposed to do the callbacks...guess it's CSO
 
     let theRemarks: IRemarks;
     theRemarks = {
