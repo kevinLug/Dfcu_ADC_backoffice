246bd9c36ce6d0d61c2af167be59fcc65d891c49
Daniel Comboni <daniel@laboremus.no>
2021-07-12 11:47:44 +0300

    removed unnecessary extra details in view

--- src/modules/scan/validate-verify/BeneficiaryDetails.tsx
+++ src/modules/scan/validate-verify/BeneficiaryDetails.tsx
@@ -27,20 +27,20 @@ const BeneficiaryDetails = () => {
 
         <Grid container>
 
-            {
-                populateLabelAndValue(["Branch", "Rate", "Currency", "Amount", "UGX Amount", 'Amount in words'],
-                    transferDetailsLabels(transferDetails, aCase), "-").map((kv, index) => {
-                    return <Grid key={index} container item spacing={4} sm={12}>
-                        <Grid item sm={5}>
-                            <DataLabel noColon={true}>{kv.key}</DataLabel>
-                        </Grid>
-                        <Grid item sm={7}>
-                            <DataValue>{kv.value}</DataValue>
-                        </Grid>
-                    </Grid>
-                })
+            {/*{*/}
+            {/*    populateLabelAndValue(["Branch", "Rate", "Currency", "Amount", "UGX Amount", 'Amount in words'],*/}
+            {/*        transferDetailsLabels(transferDetails, aCase), "-").map((kv, index) => {*/}
+            {/*        return <Grid key={index} container item spacing={4} sm={12}>*/}
+            {/*            <Grid item sm={5}>*/}
+            {/*                <DataLabel noColon={true}>{kv.key}</DataLabel>*/}
+            {/*            </Grid>*/}
+            {/*            <Grid item sm={7}>*/}
+            {/*                <DataValue>{kv.value}</DataValue>*/}
+            {/*            </Grid>*/}
+            {/*        </Grid>*/}
+            {/*    })*/}
 
-            }
+            {/*}*/}
 
             {
                 populateLabelAndValue(['Country','Plot','Building', 'Town', 'Country Code'], beneficiaryDetailsLabels(beneficiaryDetails, bankDetails), "-").map((kv, index) => {
