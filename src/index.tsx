5d7bac4c9770eeaa702516f6f2bdd1577b84fcd2
danielLaboremus <daniel@laboremus.no>
2021-10-20 16:18:48 +0300

    returned back the serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import {ThemeProvider,} from '@material-ui/styles';
+import { Provider } from 'react-redux'
+import { ThemeProvider, } from '@material-ui/styles';
 import 'react-toastify/dist/ReactToastify.css';
 import store from "./data/redux/store";
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import theme from "./theme";
 
 ReactDOM.render(
     <Provider store={store}>
         <ThemeProvider theme={theme}>
-            <App/>
+            <App />
         </ThemeProvider>
     </Provider>, document.getElementById('root'));
 
-// serviceWorker.register({
-//     onUpdate: registration => {
-//         if (window.confirm("New content available, update now?")) {
-//             window.location.reload()
-//             if (registration.waiting)
-//                 registration.waiting.postMessage({type: 'SKIP_WAITING'});
-//         }
-//     }
-// });
+serviceWorker.register({
+    onUpdate: registration => {
+        if (window.confirm("New content available, update now?")) {
+            window.location.reload()
+            if (registration.waiting)
+                registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+        }
+    }
+});
