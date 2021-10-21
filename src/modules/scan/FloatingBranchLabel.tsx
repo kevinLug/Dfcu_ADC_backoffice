25f3355babfc6dc9c38cb0d2cf2c0bf99af17ba6
danielLaboremus <daniel@laboremus.no>
2021-10-21 13:34:29 +0300

    added branch label (floating btn as label)

---
+++ src/modules/scan/FloatingBranchLabel.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+
+import DataAccessConfigs from '../../data/dataAccessConfigs';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            margin: 0,
+            top: "auto",
+            right: 'auto',
+            bottom: 20,
+            left: 20,
+            position: 'fixed',
+        },
+        extendedIcon: {
+            marginRight: theme.spacing(1),
+        },
+        serverIsReachable: {
+            color: "green"
+        },
+        serverIsNotReachable: {
+            color: "red"
+        }
+    }),
+);
+
+export default function FloatingBranchLabel() {
+
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+
+            <Fab variant="extended" className={classes.serverIsReachable} aria-label="add">
+                {DataAccessConfigs.getBranchName()}
+            </Fab>
+
+        </div>
+
+    );
+}
